253d680f1bcad71d6037f839fe90676b9f9a4ec4
mphat-phan <phanminhphat2001@gmail.com>
2023-04-15 11:53:12 +0700

    Set up vitest

---
+++ client/src/utils/isOdd.test.ts
@@ -0,0 +1,6 @@
+import { isOdd } from "./isOdd";
+import { test, expect } from "vitest";
+test('use jsdom in this test file', () => {
+    const element = isOdd(2);
+    expect(element).toEqual(false) 
+})
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,12 +1,17 @@
-import { defineConfig } from 'vite'
+/// <reference types="vitest" />
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
-
+import { defineConfig } from 'vitest/config'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000
+  },
+  test: {
+    environment: 'jsdom',
+    setupFiles: ['./tests/vitest/setup.ts'],
+    globals: false,
   },
   css: {
     devSourcemap: true
